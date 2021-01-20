9912c45e7cfeb59094fb7998213bf8569777a3aa
Marcin Robak <mr.robak@gmail.com>
2021-01-20 19:32:31 +0100

    install axios, fetch theater data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import Booking from "./pages/Booking";
 
+import { API_URL } from "./secrets/secrets";
+import "./App.css";
+
 function App() {
+  const [theater, setTheater] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/event-layout`)
+      .then((res) => {
+        setTheater(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Booking />
+      <Booking theater={theater} />
     </div>
   );
 }
